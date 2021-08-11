aa5ea8ad1043344833787149b8578e802df095df
Prince Kumar <princebantikumar@gmail.com>
2021-08-11 22:45:49 +0530

    Added delete operation and error reporting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,10 +6,32 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 	def new
+		@article = Article.new
 	end
 	def create
 		@article = Article.new(params.require(:article).permit(:title, :description))
-		@article.save
+		if @article.save
+			flash[:notice] = "Article was created successfully"
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(params.require(:article).permit(:title, :description))
+			flash[:notice] = "Article was updated successfully."
+      		redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
 		redirect_to @article
 	end
 end
